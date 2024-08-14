f43e7354c4502131aa1682fe43fd21a042dbce85
Alorican <adrianluiscaoile@gmail.com>
2024-08-14 18:00:22 +0800

    Help hahahhahahahaa

--- Movements/Byrne Assets/Byrne.gd
+++ Movements/Byrne Assets/Byrne.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 300.0
 @export var is_right: bool = true
 var is_dashing: bool = false
 var can_dash: bool = true
 var dash_cd: bool = false
 var landed: bool = true
+var is_attacking: bool = false  # Track if the character is currently attacking
 
 @export var jump_height : float
 @export var jump_time_to_peak: float
@@ -17,9 +17,8 @@ var landed: bool = true
 @onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
+var has_double_jumped: bool = false
 
 func jump():
 	$AnimatedSprite2D.play("Byrne Jump")
@@ -28,63 +27,67 @@ func jump():
 func double_jump():
 	velocity.y = double_jump_velocity
 
+func attack():
+	if is_attacking:
+		return  # Prevent attacking if already attacking
+	is_attacking = true
+	$AnimatedSprite2D.play("Byrne Sword")  # Play the attack animation
+	# After the attack animation finishes, set is_attacking to false
+	#yield($AnimatedSprite2D, "animation_finished")
+	is_attacking = false
+
 func _physics_process(delta):
-	#print(position.y)
-	if velocity.x <= 300 && velocity.x >= -200 && is_on_floor():
-		is_dashing = false;
+	if velocity.x <= 300 and velocity.x >= -200 and is_on_floor() and not is_attacking:
+		is_dashing = false
 		$AnimatedSprite2D.play("Byrne Idle")
 		
 	if is_on_floor():
 		landed = true
 	
-	if landed && !dash_cd:
-		can_dash = true	
+	if landed and not dash_cd:
+		can_dash = true    
 	
 	if is_dashing:
 		velocity.y = 975
 	else:
 		velocity.y += get_gravity() * delta
-	#print(get_gravity())
 		
-	# Add the gravity.
 	if is_on_floor():
-		has_double_jumped = false;
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump"):
+		has_double_jumped = false
+	
+	# Handle Jump
+	if Input.is_action_just_pressed("Jump") and not is_attacking:
 		if is_on_floor():
-			#Normal jump from floor
-			jump();
+			jump()
 		elif not has_double_jumped:
-			# Double jump if it hasn't been used yet
 			double_jump()
-			# Can also try using
-			# velocity.y += double_jump_velocity
-			has_double_jumped = true;
-			
-		
+			has_double_jumped = true
+
+	# Handle Attack
+	if Input.is_action_just_pressed("Attack") and not is_attacking:
+		attack()
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
 	
 	if direction < 0:
 		is_right = true
 	elif direction > 0:
 		is_right = false
-	if direction && !is_dashing:
+	
+	if direction and not is_dashing and not is_attacking:
 		velocity.x = direction * speed
 		$AnimatedSprite2D.play("Byrne Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
-	if Input.is_action_just_pressed("dash") && can_dash && !dash_cd:
+	if Input.is_action_just_pressed("dash") and can_dash and not dash_cd:
 		$AnimatedSprite2D.play("Byrne Dash")
 		if is_right:
-			velocity.x = velocity.x -4500
-		elif !is_right:
-			velocity.x = velocity.x +4500
+			velocity.x -= 4500
+		else:
+			velocity.x += 4500
 		is_dashing = true
-		#can_dash = false
-		#dash_cd = true
 		landed = false
 				
 	# Rotate
@@ -92,16 +95,11 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = true
 	elif direction == -1:
 		$AnimatedSprite2D.flip_h = false
+	
 	move_and_slide()
-
-
-
 
 func get_gravity() -> float:
 	return jump_gravity if velocity.y < 0 else fall_gravity
 
-
 func _on_dash_timer_timeout():
 	dash_cd = false
-
-
