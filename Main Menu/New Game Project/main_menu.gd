c7e34ab0c18f3e951a176f716ab1263e8f39ace2
Alorican <adrianluiscaoile@gmail.com>
2024-08-15 00:01:55 +0800

    Basic Main Menu Shit

---
+++ Main Menu/New Game Project/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Options.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().quit()
+
+func _on_button_3_pressed():
+	pass
